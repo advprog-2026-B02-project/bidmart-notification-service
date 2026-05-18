45a1d411d61c582287de36a68e56a4e70c99a18c
newjeasnz <jesiccatandra@gmail.com>
2026-05-18 16:00:32 +0700

    feat: add spring kafka dependency and kafka configuration

--- src/main/java/id/ac/ui/cs/advprog/bidmart/notifications/NotificationServiceApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/notifications/NotificationServiceApplication.java
@@ -2,8 +2,10 @@ package id.ac.ui.cs.advprog.bidmart.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
